dc0042ec4ffae7437361f426233be60a6c8fef32
Maxime <maxime.lefrancois@emse.fr>
2025-11-26 11:38:21 +0100

    remove ns. needs to be discussed in github.com/w3c/ns

--- ns/README.md
+++
@@ -1,4 +0,0 @@
-# ns
-
-Specification of the w3.org/ns/sosa and w3.org/ns/ssn spaces.
-
--- ns/script.sh
+++
@@ -1,6 +0,0 @@
-# this script would populate the .ttl and .rdf files in the 2023 folder with the graphs that exist,
-# and generate the redirections for the individual terms:
-
-# with the idea that the first match found gets is executed, we'll implement:
-# 302 to 2023 folder for terms that are part of 2023
-# 302 to 2017 folder for terms that are part of 2017 (and are not matched yet)
